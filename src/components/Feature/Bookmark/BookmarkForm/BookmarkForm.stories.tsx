f16ea2738d25718fcbdb6e3cf7afc6f20a0d3d79
shinya-creative <shinya.creative@gmail.com>
2026-03-30 15:26:34 +0900

    fix: ストーリーテンプレートの追加

---
+++ src/components/Feature/Bookmark/BookmarkForm/BookmarkForm.stories.tsx
@@ -0,0 +1,10 @@
+import type { Meta } from '@storybook/react';
+// import { BookmarkForm } from './BookmarkForm';
+
+const meta = {
+    title: 'Feature/BookmarkForm',
+    // component: BookmarkForm,
+} satisfies Meta;
+
+export default meta;
+// ↑ Storybookはこの `export default` を探していたクマ！
---
+++ src/components/Feature/Bookmark/BookmarkList/BookmarkList.stories.tsx
@@ -0,0 +1,10 @@
+import type { Meta } from '@storybook/react';
+// import { BookmarkForm } from './BookmarkForm';
+
+const meta = {
+    title: 'Feature/BookmarkList',
+    // component: BookmarkForm,
+} satisfies Meta;
+
+export default meta;
+// ↑ Storybookはこの `export default` を探していたクマ！
